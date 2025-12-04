c225df33b8bf69fe4c05292a3922ea29d125bd01
pasqualevassallo <pasquale.vass22@gmail.com>
2025-12-04 18:59:15 +0100

    Update filename for loading solutions

--- solution_analyzer.py
+++ solution_analyzer.py
@@ -1009,7 +1009,7 @@ def export_to_csv(xs, ys, filename='soluzioni_pareto.csv'):
 if __name__ == '__main__':
     
     # CARICA IL FILE (sostituisci con il tuo file .npz)
-    filename = 'quantcomm_1_100_6372134.npz'  # O il file più recente generato
+    filename = 'quantcomm_639739.npz'  # O il file più recente generato
     
     try:
         xs, ys = load_solutions(filename)
